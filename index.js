22be1d396cb3eb2aca89d123e872e01c05622680
Darakshantaj <darakshantaj1@gmail.com>
2024-04-28 12:27:47 +0500

    My Class Assignment of JavaScriptdated:28th April,2024

---
+++ index.js
@@ -0,0 +1,32 @@
+//Name:Darakshan Taj,Roll No.132005,Trainer Name:Ma'am Faiza Aziz Khan
+// Class Assignment
+
+
+//Q:3reverse the word
+var string = prompt ("please enter a string");
+var strLen = string.length;
+revStr = "";
+for (var i = strLen-1;i>=0;i--){
+    revStr +- string[i];
+
+}
+console.log('${string} reverse is ${revStr}')
+
+// Question # 04: Write a JavaScript program to display the reading status (i.e. display
+//     book name, author name and reading status) of the following books.
+
+var library = [{
+    author:
+    title:
+    readingStatus:
+ },
+{
+    author:
+    title:
+    readingStatus:
+},
+{
+    author:
+    title:
+    readingStatus:
+}];
